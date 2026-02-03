a5962975cecbd23edf6aa11c2711c41cb9388679
lordagustin22 <niconecrofilo589@gmail.com>
2026-02-03 16:32:22 -0300

    Testing permisos y el if

--- .github/workflows/rename-yaml.sh
+++ .github/workflows/rename-yaml.sh
@@ -3,10 +3,11 @@
 set -e
 
 count=$(ls -1 main-*.bak > /dev/null 2>&1 | wc -l)
+add=$((count+1))
 
 if [ $count -eq 0 ]; then
     count=1
 fi
 
-mv main.yml main-${count}.bak
+mv main.y*ml main-${add}.bak
 mv workflow-main* main.yaml
